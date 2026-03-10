2f36c7555643e99a8e16f203ddf6eead4a53264c
Ma-dyuki <dyuki.haitun505@gmail.com>
2026-03-11 06:01:00 +0800

    change background

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,21 @@ import { RouterView } from "vue-router";
 
 <template>
   <div class="root-shell">
+
+      <div class="bg-decor">
+      <img src="/decor/heart.svg" class="bg-icon i1" alt="" />
+      <img src="/decor/mug.svg" class="bg-icon i2" alt="" />
+      <img src="/decor/headphones.svg" class="bg-icon i3" alt="" />
+      <img src="/decor/glass.svg" class="bg-icon i4" alt="" />
+      <img src="/decor/reddit.svg" class="bg-icon i5" alt="" />
+      <img src="/decor/awesome.svg" class="bg-icon i6" alt="" />
+      <img src="/decor/fan.svg" class="bg-icon i7" alt="" />
+      <img src="/decor/cat.svg" class="bg-icon i8" alt="" />
+      <img src="/decor/emoji.svg" class="bg-icon i9" alt="" />
+    </div>
+
     <RouterView />
+
   </div>
 </template>
+
